fbf667dda9f99828761d15ea77f3900ce760989b
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2025-12-13 16:47:42 +0530

    updated grid for the box

---
+++ src/components/boxes.js
@@ -0,0 +1,67 @@
+const Boxex = [
+  {
+    id: 1,
+    on: true,
+  },
+  {
+    id: 2,
+    on: false,
+  },
+  {
+    id: 3,
+    on: true,
+  },
+  {
+    id: 4,
+    on: true,
+  },
+  {
+    id: 5,
+    on: false,
+  },
+  {
+    id: 6,
+    on: true,
+  },
+  {
+    id: 7,
+    on: false,
+  },
+  {
+    id: 8,
+    on: true,
+  },
+  {
+    id: 9,
+    on: false,
+  },
+  {
+    id: 10,
+    on: true,
+  },
+  {
+    id: 11,
+    on: false,
+  },
+  {
+    id: 12,
+    on: false,
+  },
+  {
+    id: 13,
+    on: true,
+  },
+  {
+    id: 14,
+    on: false,
+  },
+  {
+    id: 15,
+    on: true,
+  },
+  {
+    id: 16,
+    on: false,
+  },
+];
+export default Boxex;
