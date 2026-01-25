055b12737fb8e7eb86a38a402e57af91d8faf9f9
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2026-01-26 02:40:30 +0530

    Footer mobile optimization
    Footer mobile optimization

--- juice-3d/src/components/SixFooter.tsx
+++ juice-3d/src/components/SixFooter.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image';
 export default function Footer() {
   return (
     /* FOOTER */
-    <footer className="pt-20 pb-10 border-t border-gray-100">
-      <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-4 gap-12 text-sm text-gray-500">
+    <footer className="pt-6 md:pt-20 pb-6 md:pb-10 border-t border-gray-100">
+      <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-4 gap-6 md:gap-12 text-sm text-gray-500">
         <div>
-          <div className="flex items-center gap-2 text-2xl font-bold text-gray-900 mb-6">
+          <div className="flex items-center gap-2 text-2xl font-bold text-gray-900 mb-4 md:mb-6">
             <div className="w-8 h-8 bg-plant-primary rounded-lg flex items-center justify-center text-white text-base">
               <Image
                 // src="/plantaLogo.png"
@@ -18,40 +18,42 @@ export default function Footer() {
             </div>
             Planta
           </div>
-          <p className="mb-6 leading-relaxed">
+          <p className="mb-4 md:mb-6 leading-relaxed">
             Your plants are thriving because of you! Water, trim, and nourish
             them to keep them happy.
           </p>
-          <h4 className="font-bold text-gray-900 mb-2">
-            Get the latest information
-          </h4>
-          <div className="bg-gray-50 p-3 rounded-full border text-center">
-            Supportplanta@gmail.com
-          </div>
         </div>
-        <div className="space-y-4">
+        <div className="space-y-2 md:space-y-4">
           <h4 className="font-bold text-gray-900">Company</h4>
           <p>Become an Agent</p>
           <p>Get Referrals</p>
           <p>Careers</p>
           <p>Our Company</p>
         </div>
-        <div className="space-y-4">
+        <div className="space-y-2 md:space-y-4">
           <h4 className="font-bold text-gray-900">Contact</h4>
           <p>Contact us</p>
           <p>Help center</p>
           <p>Advertise</p>
         </div>
-        <div className="space-y-4">
+        <div className="space-y-2 md:space-y-4">
           <h4 className="font-bold text-gray-900">About Us</h4>
           <p>Why Redfin</p>
           <p>Community Impact</p>
           <p>Diversity & Inclusion</p>
         </div>
       </div>
-      <div className="max-w-7xl mx-auto px-6 mt-16 pt-8 border-t flex flex-col md:flex-row justify-between text-xs text-gray-400">
+      <div className="max-w-7xl mx-auto px-6 mt-8 md:mt-16 pt-8 md:flex-row text-xs text-gray-400">
+        <h4 className="font-bold text-gray-900 mb-4 md:mb-2">
+          Get the latest information
+        </h4>
+        <div className="bg-gray-50 p-3 w-60 rounded-full border text-center">
+          Supportplanta@gmail.com
+        </div>
+      </div>
+      <div className="max-w-7xl mx-auto px-6 mt-8 md:mt-16 pt-8 border-t flex flex-col md:flex-row justify-between text-xs text-gray-400">
         <p>Copyright 2026. All Rights Reserved</p>
-        <div className="flex gap-6 mt-4 md:mt-0">
+        <div className="flex gap-10 md:gap-6 mt-4 md:mt-0">
           <span>Privacy Policy</span>
           <span>Terms and Use</span>
         </div>
